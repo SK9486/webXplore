ee5041de4ae6058df9526cc6c60443978a1fe844
valli74appan@gmail.com <valli74appan@gmail.com>
2024-11-10 20:57:48 +0530

    search header completed

--- src/app/components/SearchHeader.jsx
+++ src/app/components/SearchHeader.jsx
@@ -1,40 +1,38 @@
 'use client';
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
+import React from "react";
 import { RiSettings3Line } from 'react-icons/ri';
-import { TbGridDots } from 'react-icons/tb'; // Corrected import here
+import { TbGridDots } from 'react-icons/tb';
 import SearchBox from './SearchBox';
 import SearchHeaderOptions from './SearchHeaderOptions';
 
 function SearchHeader() {
-  
   return (
     <div className="flex flex-col">
       <header className="flex items-center justify-between p-4">
-      <div className="flex-w-full p-6 items-center justify-between">
-        <Link href={'/'}>
+        <Link href="/">
           <Image
             width={120}
             height={40}
-            src={"https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg"}
+            src="https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg"
             alt="Google Logo"
           />
         </Link>
-      </div>
-      <div className="flex-1">
-        <SearchBox />
-      </div>
-      <div className="hidden md:inline-flex space-x-2 ">
-        <RiSettings3Line className="header-icon"/>
-        <TbGridDots className="header-icon"/>
-      </div>
-      <button className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-all">Sign In</button>
-    </header>
-    <SearchHeaderOptions/>
+        <div className="flex-1 mx-6">
+          <SearchBox />
+        </div>
+        <div className="hidden md:flex items-center space-x-2">
+          <RiSettings3Line className="text-2xl text-gray-600 cursor-pointer" />
+          <TbGridDots className="text-2xl text-gray-600 cursor-pointer" />
+        </div>
+        <button className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-all">
+          Sign In
+        </button>
+      </header>
+      <SearchHeaderOptions />
     </div>
-    
   );
-};
+}
 
 export default SearchHeader;
--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -1,9 +1,17 @@
+
+import { useSearchParams } from 'next/navigation';
 import React from 'react'
 
-function WebSearchPage() {
+async function WebSearchPage({searchParams}) {
+  const response = await fetch(` https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`);
+  const data = await response.json();
+  console.log(data);
+  const results = data.items;
   return (
-    <div>WebSearchPage</div>
+    <>
+    {results && results.map((result) => <h1>{result.title}</h1> )}
+    </>
   )
 }
 
-export default WebSearchPage
+export default WebSearchPage;
