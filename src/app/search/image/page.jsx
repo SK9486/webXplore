ca9faf0d5f0bdc69baf1b185439132171c71df93
valli74appan@gmail.com <valli74appan@gmail.com>
2024-11-13 20:09:49 +0530

    image web section created

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
-function page() {
+'use client';
+import Link from 'next/link';
+import {useSearchParams } from 'next/navigation';
+import React from 'react';
+import ImageSearchResults from "../../components/ImageSearchResults.jsx";
+async function ImageSearchPage({searchParams}) {
+  const response = await fetch(` https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image`);
+  if(!response.ok){
+    throw new Error("Failed to Fetch Data")
+  }
+  const data = await response.json();
+  console.log(data);
+  const results = data.items;
+  if(!results){
+    return(<div className='flex flex-col justify-center items-center mt-10'>
+      <h1 className=' text-2xl text-red-500 mt-10'>No Results Found!!</h1>
+      <Link className='p-3 rounded-lg bg-blue-500 text-gray-100 hover:brightness-110 hover:text-white cursor-pointer mt-5' href={'/'}>Go Back</Link>
+    </div>)
+  }
   return (
-    <div>page</div>
+    <>
+    {results && <ImageSearchResults results={data}/>}
+    </>
   )
 }
 
-export default page
+export default ImageSearchPage;
